9a5a5520a7cff5a4a4d7f56dba06b9655fd31c43
Amit <29amitbishnoi2929@gmail.com>
2022-06-04 17:48:18 +0530

    header menu

--- bookWrench/widgets/Header/index.jsx
+++ bookWrench/widgets/Header/index.jsx
@@ -52,11 +52,24 @@ const defaultProps = {
             link: Dashboard,
             isActive: false,
             tag: 'MOD_OFFER',
+            icon: 'M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z',           
+        },
+        {
+            title: 'More',
+            link: Dashboard,
+            isActive: false,
+            tag: 'MOD_OFFER',
             icon: 'M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z',
             subMenu: [
-                { title: 'Department', link: Department, icon: '', isActive: true },
-                { title: 'Category', link: Category, icon: '', isActive: true },
-                { title: 'Sub-Category', link: SubCategory, icon: '', isActive: false },
+                { title: 'Master', link: Dashboard, icon: '', isActive: true,
+                subMenu: [
+                    { title: 'Master', link: Dashboard, icon: '', isActive: true },
+                    // { title: 'Category', link: Category, icon: '', isActive: true },
+                    // { title: 'Sub-Category', link: SubCategory, icon: '', isActive: false },
+                ]
+            },
+                // { title: 'Category', link: Category, icon: '', isActive: true },
+                // { title: 'Sub-Category', link: SubCategory, icon: '', isActive: false },
             ]
         },
     ]
