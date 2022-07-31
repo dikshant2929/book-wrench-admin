de444b7908532124a4799304b51436ae800895f8
Amit <29amitbishnoi2929@gmail.com>
2022-07-31 14:18:58 +0530

    design changes

--- bookWrench/pages/Customer/sub-module/maintenance/index.jsx
+++ bookWrench/pages/Customer/sub-module/maintenance/index.jsx
@@ -99,10 +99,10 @@ const MaintenanceCustomer = (props) => {
 
 
                 <div className='absolute top-0 right-0 flex gap-1 items-center p-2'>
-                    <span onClick={() => editPointOfContact(list.itemNumber)}>
+                    <span className='cursor-pointer' onClick={() => editPointOfContact(list.itemNumber)}>
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="#a4a4a4"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
                     </span>
-                    <span onClick={() => onRemoveMaintenance(list.id, maintenanceTitle)}>
+                    <span className='cursor-pointer' onClick={() => onRemoveMaintenance(list.id, maintenanceTitle)}>
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="#a4a4a4"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
                     </span>
                 </div>
